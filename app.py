72e5336a961c27333282dd737b31d9eb11ea1e3e
Lucas Zoser Nunes Costa <lucaszoser@dinfor.uniceub>
2023-09-11 19:41:41 -0300

    criando home

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+
+@app.route('//')
+def home():
     return render_template('index.html')
+
+@app.route('/jogo')
+def index():
+    return render_template('jogo.html')
 
 @app.route('/resultado', methods=['POST'])
 def resultado():
@@ -14,7 +19,6 @@ def resultado():
 
     if selected_edges_json:
         selected_edges = json.loads(selected_edges_json)
-        #print(selected_edges)
         G = nx.DiGraph()
         for edge in selected_edges:
             print(edge)
@@ -22,25 +26,20 @@ def resultado():
             weight = edge['weight']
             G.add_edge(name[0], name[1], weight=int(weight))
 
-        # Use o algoritmo de Kruskal para calcular a MST
         mst = nx.minimum_spanning_tree(G.to_undirected())
 
-        # Crie uma lista de arestas da MST
         mst_edges = []
         for edge in mst.edges(data=True):
             name = f"{edge[0]}{edge[1]}"
             weight = edge[2]['weight']
             mst_edges.append({'name': name, 'weight': weight})
             print(mst_edges)
-        # Ordene as arestas por peso
+
         mst_edges.sort(key=lambda x: x['weight'])
 
-        # Verificação de ciclo pelo nome
         def has_cycle(graph, edge):
             return nx.has_path(graph, edge['name'][1], edge['name'][0])
 
-        # Remova arestas que formam ciclos
-        #mst_edges = [edge for edge in mst_edges if not has_cycle(mst, edge)]
         if len(mst_edges) != 4:
             return '<h1>O mestre Kruskal não conseguiu encontrar o tesouro, tente novamente</h1>'
         return render_template('resultado.html', mst_edges=mst_edges)
