6ec6c6480025dbc01227f74ef668d08d0dcd4ad1
Lucas Zoser <53053622+Zosin0@users.noreply.github.com>
2023-09-18 11:00:47 -0300

    almost finished ´- steps and tips

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import json
 import math
 
 app = Flask(__name__)
-app.config['CACHE_TYPE'] = 'simple'  
+app.config['CACHE_TYPE'] = 'simple'
 cache = Cache(app)
+
 
 def calcular_angulo(ponto1, ponto2):
     return math.degrees(math.atan2(ponto2['y'] - ponto1['y'], ponto2['x'] - ponto1['x']))
@@ -16,88 +17,123 @@ def calcular_angulo(ponto1, ponto2):
 def home():
     return render_template('index.html')
 
+
 @app.route('/jogo')
 def index():
     return render_template('jogo.html')
+
 
 @app.route('/passo1')
 def passo1():
     return render_template('passo1.html')
 
+
 @app.route('/passo2', methods=['POST'])
 def passo2():
-    global selected_points
     points_data = request.form.get('points')
+    global selected_points
 
     selected_points = json.loads(points_data)
+    print(selected_points)
     cache.set('selected_points', selected_points)
 
     return render_template('passo2.html', pontos=selected_points)
+
+
+def formatar_estilo(estilo):
+    estilo_formatado = ''
+    for propriedade in estilo:
+        propriedade_formatada = propriedade.strip('\'" ')
+        valor_formatado = estilo[propriedade].strip('\'" ')
+        if propriedade_formatada == 'backgroundColor':
+            propriedade_formatada = 'background-color'
+        estilo_formatado += f"{propriedade_formatada}: {valor_formatado}; "
+    return estilo_formatado
+
 
 @app.route('/passo3', methods=['POST'])
 def passo3():
     arestas_data = request.form['edges']
     arestas = json.loads(arestas_data)
     selected_points = cache.get('selected_points')
-
-    global selected_edges
     arestas_s = json.loads(arestas_data)
     cache.set('arestas_s', arestas_s)
 
-    def calcular_distancia(ponto1, ponto2):
-        diff_x = (ponto2['x']-2) - (ponto1['x']-2)
-        diff_y = (ponto2['y']-2) - (ponto1['y']-2)
-        
-        distancia = math.sqrt(diff_x ** 2 + diff_y ** 2)
-        
-        return distancia
-
     for aresta in arestas:
-        aresta['angulo'] = calcular_angulo(aresta['ponto1'], aresta['ponto2'])
-        aresta['largura'] = calcular_distancia(aresta['ponto1'], aresta['ponto2'])  
+        estilo_formatado = formatar_estilo(aresta['style'])
+        aresta['style'] = estilo_formatado
+        print(estilo_formatado)
 
-    if not selected_points:
-        return "Nenhum ponto selecionado."
+    return render_template('passo3.html', pontos=selected_points, arestas=arestas)
 
-    return render_template('passo3.html', pontos=selected_points, arestas=arestas)  
+
+def has_cycle(graph, edge):
+    return nx.has_path(graph, edge['name'][1], edge['name'][0])
+
+def find_longest_path(graph, start):
+    def dfs(node, path):
+        nonlocal longest_path
+        path.append(node)
+        for neighbor in graph.neighbors(node):
+            if neighbor not in path:
+                dfs(neighbor, path)
+        if len(path) > len(longest_path):
+            longest_path = path.copy()
+
+    longest_path = []
+    dfs(start, [])
+    return longest_path
 
 @app.route('/passo4', methods=['POST'])
 def passo4():
     selected_edges_json = request.form.get('selectedEdges')
     selected_points = cache.get('selected_points')
-    #arestas = cache.get('arestas_s')
 
     if selected_edges_json:
         selected_edges = json.loads(selected_edges_json)
-        G = nx.DiGraph()
+
+        # Crie um grafo ponderado não direcionado
+        G = nx.Graph()
         for edge in selected_edges:
-            print(edge)
             name = edge['name']
             weight = edge['weight']
             estilo = edge['estilo']
             G.add_edge(name[0], name[1], weight=int(weight), estilo=estilo)
 
-        mst = nx.minimum_spanning_tree(G.to_undirected())
+        # Calcule a MST usando o algoritmo de Kruskal
+        mst = nx.minimum_spanning_tree(G)
 
+        # Extraia as arestas da MST
         mst_edges = []
-        for edge in mst.edges(data=True):
-            name = f"{edge[0]}"
-            weight = edge[1]
-            estilo = f"{(edge[2])}"
-            mst_edges.append({'name': name, 'weight': weight, 'estilo': estilo})
+        for u, v, data in mst.edges(data=True):
+            weight = data['weight']
+            estilo = data['estilo']
+            mst_edges.append({'name': (u, v), 'weight': weight, 'estilo': estilo})
 
         mst_edges.sort(key=lambda x: x['weight'])
 
-        def has_cycle(graph, edge):
-            return nx.has_path(graph, edge['name'][1], edge['name'][0])
+        # Encontre o caminho mais longo
+        start_node = list(G.nodes())[0]
+        longest_path_edges = find_longest_path(G, start_node)
 
-        print(mst_edges)
-        print(selected_points)
-        #print(arestas)
+        # Encontre os ciclos evitados
+        cycle_edges = []
+        for cycle in nx.simple_cycles(G):
+            if len(cycle) > 3:
+                for i in range(len(cycle) - 1):
+                    edge_name = (cycle[i], cycle[i + 1])
+                    weight = G.get_edge_data(*edge_name)['weight']
+                    estilo = G.get_edge_data(*edge_name)['estilo']
+                    cycle_edges.append({'name': edge_name, 'weight': weight, 'estilo': estilo})
+                edge_name = (cycle[-1], cycle[0])
+                weight = G.get_edge_data(*edge_name)['weight']
+                estilo = G.get_edge_data(*edge_name)['estilo']
+                cycle_edges.append({'name': edge_name, 'weight': weight, 'estilo': estilo})
 
-        return render_template('passo4.html', mst_edges=mst_edges, pontos=selected_points)
+        return render_template('passo4.html', mst_edges=mst_edges, longest_path_edges=longest_path_edges, cycle_edges=cycle_edges, pontos=selected_points)
 
-
+    else:
+        return render_template('passo4_no_selection.html')
 
 @app.route('/resultado', methods=['POST'])
 def resultado():
@@ -129,9 +165,10 @@ def resultado():
         if len(mst_edges) != 4:
             return '<h1>O mestre Kruskal não conseguiu encontrar o tesouro, tente novamente</h1>'
         return render_template('resultado.html', mst_edges=mst_edges)
-        
+
     else:
         return "Nenhuma aresta selecionada."
-    
+
+
 if __name__ == '__main__':
     app.run(debug=True)
